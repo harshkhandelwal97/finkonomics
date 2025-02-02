2c1cc6a4153b6acd115d38c56ec5d4e09afc2965
an-ash-iith <anashwinkr@gmail.com>
2025-02-02 20:16:01 +0530

    fix (ui) : otp screen

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,33 @@
 import './App.css';
 // import CompanyCard from './components/companycard';
 // import landingPage from './pages/LandingPage';\
-// import LandingPage from './pages/LandingPage';
-import LoginPage from './pages/loginPage';
-
+// import Permission from './pages/Permission';
+// import LoginPage from './pages/loginPage';
+// import HistoryPage from './pages/HistoryPage';
+// import  Homepage  from  './pages/HomePage'
+import { Homepage } from './pages/HomePage';
+import CompanyLoginPage from './pages/CompanyLoginPage';
+import URegistrationForm from './pages/UserRegistration';
+import PhoneNumberScreen from './pages/UserPhoneRegistration';
+import OTPScreenPhone from './pages/UserPhoneOtp';
+import OTPScreen from './pages/UserEmailOtp';
 function App()
 {
 
   return (
     <div className='App'>
-      {/* <LandingPage /> */}
-      <LoginPage/>
+      {/* <Permission /> */}
+      {/* <LoginPage/> */}
+      {/* <HistoryPage/> */}
+      {/* <Homepage
+        totalPoints={9875}
+        totalValue="₹6325"
+        savedAmount="₹5467"
+      /> */}
+      {/* <CompanyLoginPage/> */}
+      {/* <URegistrationForm/> */}
+      {/* <PhoneNumberScreen /> */}
+      <OTPScreen/>
     </div>
   );
 }
