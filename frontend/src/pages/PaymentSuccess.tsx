8203171e07f6c008da839db2f787607d5155b8ce
jais_vicky <ce21btech11045@iith.ac.in>
2025-02-05 06:17:07 +0530

    fix: (ui) payment success page

--- frontend/src/pages/PaymentSuccess.tsx
+++ frontend/src/pages/PaymentSuccess.tsx
@@ -1,5 +1,20 @@
+import { useEffect } from "react"
+import { useNavigate } from "react-router-dom";
+
 export default function PaymentSuccess() {
-    return(
+
+    const navigate = useNavigate();
+    useEffect(() => {
+
+        const timer = setTimeout(() => {
+            navigate("/");
+        }, 3000);
+
+        return () => clearTimeout(timer);
+
+    }, [navigate])
+
+    return (
         <div>
             Payment Successfull
         </div>
